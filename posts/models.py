d13e213ee388e6f93529b02bcc685061c16b7f32
abdo1fraza <abdo87.hamza@gmail.com>
2023-01-13 21:37:45 +0200

    add static and media file confg into setting.py

--- posts/models.py
+++ posts/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     title = models.CharField(max_length = 100)
     content = models.TextField(max_length = 1000)
     created_at = models.DateField(default = timezone.now)
-    image = models.ImageField(upload_to='posts_img/')
+    image = models.ImageField(upload_to='posts_image/')### make folder posts_image in media folder and store images in it 
 
 
     def __str__(self):
--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,8 +117,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+## when I am trying to make static like documentatio 
+# not work
+#but when make os.path.join 
+#its work
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR , "static"),
+]
 
-STATIC_URL = 'static/'
+MEDIA_ROOT = os.path.join(BASE_DIR , "media")
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
